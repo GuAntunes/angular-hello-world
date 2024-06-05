1f3ba4f5399d2db3b6bd29dbf29a2ec9d2590a49
Gustavo Antunes <gustavo.silva@ztech.net>
2024-06-04 21:56:54 -0300

    Created new component home

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'angular-hello-world' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-hello-world');
+    expect(app.title).toEqual('Angular Hello World');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-hello-world');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Angular Hello World');
   });
 });
